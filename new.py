a915cab162523277ee535c41ecde4fabfd0bce96
pavan318-admin <pavankonidala2000@gmail.com>
2026-09-21 21:34:21 +0530

    changes saved

--- new.py
+++ new.py
@@ -1,12 +1,27 @@
-name = "kpkalyan"
-age = 26 
-height = 5.7
-weight = 65
-is_employee = True
+# #name = "kpkalyan"
+# age = 26 
+# height = 5.7
+# weight = 65
+# is_employee = True
 
-print("hi , hello!")
-print(name)
-print(age)
-print(height)
-print(weight)
-print(is_employee)
+# print("hi , hello!")
+# print(name)
+# print(age)
+# print(height)
+# print(weight)
+# #print(is_employee)
+
+# USER INPUT:
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# height = float(input("Enter your height: "))
+# weight = int(input("Enter your weight: "))
+# is_employee = input("Are you an employee? (yes/no): ") == "yes"
+# salary = float(input("Enter your salary: "))
+
+AGE = int(input("Enter your age: "))
+if AGE >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
